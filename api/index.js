0b3ebefb5be75b88dc6e0a24dcb6ce15a1924d0c
SamuelRnn <ricardo.reinn@outlook.com>
2022-11-20 12:19:30 -0500

    20-11-22 - config

--- api/index.js
+++ api/index.js
@@ -22,6 +22,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 const PORT = process.env.PORT || 3001
+
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
